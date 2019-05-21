8be9e6fe5b66b1dc31d4d7926cdaa5e7756a8e12
Andrew Flood <andrew.flood@mail.utoronto.ca>
2019-05-21 17:05:23 -0400

    added a couple index function tests

--- tests/test_indexfunc.py
+++ tests/test_indexfunc.py
@@ -5,7 +5,28 @@ import unittest
 import numpy.testing as npt
 import os
 
+class TestSingleIndexFunction(unittest.TestCase):
 
+    def test_single_index_real(self):
+        index_fcn = sm.single_index_function(1.5)
+        self.assertAlmostEqual(index_fcn(sm.Units.um), 1.5)
+
+class TestMixedMediumFunction(unittest.TestCase):
+    def test_bruggemann_mixed_medium(self):
+        index1_fcn = sm.single_index_function(1.5)
+        index2_fcn = sm.single_index_function(1.0)
+        brug_fcn = sm.generate_mixed_medium_approximation(0.5, index1_fcn,
+                                                          index2_fcn,
+                                                          mix_type = "bruggeman")
+        self.assertAlmostEqual(brug_fcn(sm.Units.um), 1.233182491)
+
+    def test_MG_mixed_medium(self):
+        index1_fcn = sm.single_index_function(1.5)
+        index2_fcn = sm.single_index_function(1.0)
+        brug_fcn = sm.generate_mixed_medium_approximation(0.5, index1_fcn,
+                                                          index2_fcn,
+                                                          mix_type = "MG")
+        self.assertAlmostEqual(brug_fcn(sm.Units.um), 1.249489691)
 
 if __name__ == '__main__':
     unittest.main()
